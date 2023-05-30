0c461e1d7c3c5137618aa36ed7ec1f628ca1d76a
Pheuw1 <gabrieldelaflotte@gmail.com>
2023-05-30 19:11:02 +0200

    ---

--- ex05.py
+++ ex05.py
@@ -9,55 +9,6 @@ class Node:
         self.left = left
         self.right = right
         self.parent = parent
-        
-    def display(self):
-        lines, *_ = self._display_aux()
-        for line in lines:
-            print(line)
-
-    def _display_aux(self):
-        # No child.
-        if self.right is None and self.left is None:
-            line = '%s' % self.value
-            width = len(line)
-            height = 1
-            middle = width // 2
-            return [line], width, height, middle
-
-        # Only left child.
-        if self.right is None:
-            lines, n, p, x = self.left._display_aux()
-            s = '%s' % self.value
-            u = len(s)
-            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
-            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
-            shifted_lines = [line + u * ' ' for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
-
-        # Only right child.
-        if self.left is None:
-            lines, n, p, x = self.right._display_aux()
-            s = '%s' % self.value
-            u = len(s)
-            first_line = s + x * '_' + (n - x) * ' '
-            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
-            shifted_lines = [u * ' ' + line for line in lines]
-            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
-
-        # Two children.
-        left, n, p, x = self.left._display_aux()
-        right, m, q, y = self.right._display_aux()
-        s = '%s' % self.value
-        u = len(s)
-        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
-        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
-        if p < q:
-            left += [n * ' '] * (q - p)
-        elif q < p:
-            right += [m * ' '] * (p - q)
-        zipped_lines = zip(left, right)
-        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
-        return lines, n + m + u, max(p, q) + 2, n + u // 2
 
 def build_ast(expression):
     stack = []
@@ -113,29 +64,6 @@ def negation(n : Node):
             n.left.right = Node("!", n.left.right, None, n.left)
             n.left.parent = n.parent
             n.value = ""
-
-
-def group_ands(n : Node):
-    if n.value == "&":
-        if n.left.value == "&":
-            n.left = n.left.left
-            n.left.parent = n
-            group_ands(n)
-        if n.right.value == "&":
-            n.right = n.right.right
-            n.right.parent = n
-            group_ands(n)
-
-def group_ors(n : Node):
-    if n.value == "|":
-        if n.left.value == "|":
-            n.left = n.left.left
-            n.left.parent = n
-            group_ors(n)
-        if n.right.value == "|":
-            n.right = n.right.right
-            n.right.parent = n
-            group_ors(n)
         
 def convert_tree(n : Node):
     exclusive_or(n)
@@ -169,34 +97,36 @@ def remove_empty(node):
         remove_empty(node.right)
         
 
-
 def negation_normal_form(expression : str) -> str:
     root = build_ast(expression)
-    # root.display()
     convert_tree(root)
     remove_empty(root)
     if root.value == "":
         root = root.left
         root.parent = None
-    # root.display()
 
     return to_expression(root)
     
-expression = "ab&!a&!b&!"
-expressions =  [
-                "AB&!", 
-                "AB|!", 
-                "AB>", 
-                "AB=",
-                "AB|C&!",
-                "AB|C|D|",
-                "AB&C&D&",
-                "AB|!C!&"
-                ]
+
+def main():
+        
+    expression = "ab&!a&!b&!"
+    expressions =  [
+                    "AB&!", 
+                    "AB|!", 
+                    "AB>", 
+                    "AB=",
+                    "AB|C&!",
+                    "AB|C|D|",
+                    "AB&C&D&",
+                    "AB|!C!&"
+                    ]
 
 
-print("### Testing NNF ###")
-for e in expressions:
-    print(f"{e} NNF-> {negation_normal_form(e)}")
-    
+    print("### Testing NNF ###")
+    for e in expressions:
+        print(f"{e} NNF-> {negation_normal_form(e)}")
+        
 
+if __name__ == "__main__":
+    main()
